b3596b9964b70e609c09cdbaed0c87949202dd52
Myroslava-Shtylykha <mirunja@gmail.com>
2022-11-07 23:25:16 +0200

    Change link

--- src/Link.js
+++ src/Link.js
@@ -5,7 +5,7 @@ export default function Link() {
   return (
     <p className="github_Link">
       This project was coded by Myroslava Shtylykha and is
-      <a href="https://github.com/Myroslava-Shtylykha/project_weather">
+      <a href="https://github.com/Myroslava-Shtylykha/react-weather-app">
         {" "}
         open-source code on GitHub
       </a>
